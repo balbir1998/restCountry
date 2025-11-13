49c8e585ebfb2fab4f5f60d3010c154726c7ff9d
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-11-13 17:11:45 +0530

    display all countries functionality

---
+++ script.js
@@ -0,0 +1,65 @@
+const countriesContainer = document.querySelector(".countries-container");
+const filterByRegion = document.querySelector(".filter-by-region");
+const searchFilter = document.querySelector(".search-filter input");
+const clearFilter = document.querySelector(".clear-filter");
+
+window.onload = document.body.style.visibility = "unset";
+
+const fields = ["name", "flags", "population", "capital", "region"];
+const url = `https://restcountries.com/v3.1/all?fields=${fields.join(",")}`; //fields limit is 10
+
+fetch(url)
+    .then(res => res.json())
+    .then(countries => {
+        renderCountries(countries);
+
+        filterByRegion.addEventListener("change", function (e) {
+            const filteredCountries = countries.filter(country =>
+                country.region.toLowerCase() === e.target.value
+            );
+            renderCountries(filteredCountries);
+            filterByRegion.blur();
+            clearFilter.classList.add("active");
+        });
+
+        searchFilter.addEventListener("input", function (e) {
+            const searchedCountries = countries.filter(country =>
+                country.name.common.toLowerCase().includes(e.target.value)
+            );
+            renderCountries(searchedCountries);
+        });
+
+        clearFilter.addEventListener("click", function () {
+            clearFilter.classList.remove("active");
+            filterByRegion.value = "Filter by Region";
+            filterByRegion.blur();
+            renderCountries(countries);
+        });
+    })
+    .catch(err => console.log(err));
+
+function renderCountries(countries) {
+    countriesContainer.innerHTML = "";
+
+    if (!countries.length) {
+        countriesContainer.innerHTML = "Not found!";
+        return;
+    }
+
+    countries.forEach(country => {
+        const countryCard = document.createElement("a");
+        countryCard.classList.add("country-card");
+        countryCard.href = `/country.html?name=${country.name.common}`;
+
+        const cardHTML = `
+                <div class="img-container"><img src="${country.flags.svg}" alt="${country.name.common}"></div>
+                <div class="card-text">
+                    <h3 class="country-name">${country.name.common}</h3>
+                    <p class="population"><b>Population:</b> ${country.population.toLocaleString('en-IN')}</p>
+                    <p class="region"><b>Region:</b> ${country.region}</p>
+                    <p class="capital"><b>Capital:</b> ${country.capital?.[0]}</p>
+                </div>`;
+        countryCard.innerHTML = cardHTML;
+        countriesContainer.append(countryCard);
+    });
+}
